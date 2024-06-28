63af79799779f13992e22c4abba0d7e01446bbf2
Anshul3977 <palarpwaranshul@gmail.com>
2024-06-28 23:19:53 +0530

    cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 
-import { useContext, useState } from 'react';
+import { useContext, useReducer, useState } from 'react';
 import './App.css'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
 import TodoContext from './context/TodoContext';
+import todoReducer from './reducers/todoReducer';
+import TodoDispatchContext from './context/TodoDispatchContext';
 
 function App() {
-  const [list,setList] = useState(
-    [
-        {id:1 , todoData:'todo1',finished:true},
-        {id:2 , todoData:'todo2',finished:false},
-    ]
-  );
+ 
+  const [list,dispatch] = useReducer(todoReducer , []) //initial state i.e empty
   return (
     // bcoz of this value it is accessible by everyone so if addtodo mai vgre useContext and vahi same context pass kroge provider ka parent banauge so u can access
-    <TodoContext.Provider value={{list,setList}}> 
-    <AddTodo updateList ={(todo) => setList([...list , {id:list.length + 1  ,todoData:todo,finished : false}])}/>
+    <TodoContext.Provider value={{list}}> 
+    <TodoDispatchContext.Provider value={{dispatch}}>
+    <AddTodo />
       <TodoList />
+      </TodoDispatchContext.Provider>
     </TodoContext.Provider>
   )
 }
